6f07e500084c51d159073042106789e92e769d15
Truan dep troai <trantnce180829@fpt.edu.vn>
2025-09-15 13:05:41 +0700

    update code

--- Fetch_API/useEffect/src/App.jsx
+++ Fetch_API/useEffect/src/App.jsx
@@ -1,4 +1,6 @@
+import DemoEmptyDeps from "./components/DemoEmptyDeps";
 import DemoNoDeps from "./components/DemoNoDeps";
+import DemoWithDeps from "./components/DemoWithDeps";
 
 function App() {
   return (
